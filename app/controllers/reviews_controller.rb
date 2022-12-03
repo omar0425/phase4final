ca4289162a73d1012fa4f0b917dfbc45f9428edc
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-12-02 21:55:29 -0500

    Hit a roadblock with create in reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,43 @@
 class ReviewsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+  def index 
+    reviews = Review.all
+    render json: reviews
+  end
+
+  def show
+    review = find_review
+    render json: review
+  end
+
+  def create 
+    review = Review.create(review_params)
+    render json: review, status: :created
+  end
+
+  def update
+    review = find_review
+    review.update(review_params)
+    render json: review
+  end
+
+
+  private
+
+  def find_review
+    Review.find(params[:id])
+  end
+
+  def review_params
+    params.permit(
+    :comment,
+    :rating,
+    :user_id,
+    :movie_id)
+  end
+  
+  def render_not_found_response
+    render json: { error: "Review not found"}, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :movies, only: [:index,:create,:show]
-  resources :users, only: [:index,:create]
+  # resources :movies, only: [:index,:create,:show]
+  # resources :users, only: [:index,:create]
   resources :reviews, only: [:index,:create,:show,:delete]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
