ceaf70c8d11a08f023ac0e5210e42cb65c7a10d7
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-12-02 21:01:11 -0500

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,17 @@
 class MoviesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index 
     movies = Movie.all
     render json: movies
   end
 
   def show
-
     movie = find_movie
+    render json: movie
+  end
+
+  def create 
+    movie = Movie.create(movie_params)
     render json: movie
   end
 
@@ -16,7 +21,16 @@ class MoviesController < ApplicationController
   def find_movie
     Movie.find(params[:id])
     end
-
-  
+  def movie_params
+    params.permit(:title,
+    :category,
+   :poster_url,
+    :description,
+    :year,
+    :length)
+  end
+  def render_not_found_response
+    render json: { error: "Movie not found"}, status: :not_found
+  end
   
 end
