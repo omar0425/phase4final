01121d51b478e8ab71dfad608807ec2e0af3ad7e
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-12-08 13:17:52 -0500

    Set up Browser Router
    Set up Browser Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
-
-import './App.css';
-import Signup from './components/Signup';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Signup from "./pages/Signup";
+import Movies from "./pages/Movies";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
-    <div className="App">
-      <Signup />
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path="/">
+              <Movies />
+              </Route>
+            <Route exact path="/login">
+              <Login />
+              </Route>
+            <Route exact path="/signup">
+              <Signup />
+              </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
+
+//dashBoard(homepage)
+//login
+//signup
+//project
 
 export default App;
