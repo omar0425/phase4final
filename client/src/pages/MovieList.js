4d474111bd31d16d4e3e1cb5142a7c7c7fec21e9
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-12-11 16:55:24 -0500

    Update MovieList.js

--- client/src/pages/MovieList.js
+++ client/src/pages/MovieList.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import MovieCard from "../components/MovieCard";
 import { Link } from "react-router-dom";
 
-const MovieList = ({movies}) => {
+const MovieList = ({movies, user}) => {
+  const [filter, setFilter] = useState('')
+  const [filteredMovies, setFilteredMovies] = useState([])
+  useEffect(() => {
+    if (filter === '') {
+      setFilteredMovies(movies)
+    } else {
+      setFilteredMovies(user.movies)
+    }
+  }, [filter])
+  
+  function handleChange(e) {
+    setFilter(e.target.value)
+  }
 
-
-  return (
-    <div>
-      <Link to="movie/new">
-        <button >Create a New Movie</button>
-      </Link>
-      <section>
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </section>
-    </div>
-  );
-};
+  return 
+    (
+      <>
+        <select
+          onChange={handleChange}
+        >
+          <option value=''>Filter Movies by User</option>
+          <option value='myMovies'>My movies</option>
+          {/* <option value='notMyMovies'>Not my movies</option> */}
+        </select>
+        <div>
+          <Link to="movie/new">
+            <button>Create a New Movie</button>
+          </Link>
+          <section>
+            {filteredMovies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          </section>
+        </div>
+      </>
+    );
+  };
+  
 
 export default MovieList;
