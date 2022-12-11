a1c180163723b12db4e66c8b019f27405010878a
omar0425 <63131145+omar0425@users.noreply.github.com>
2022-12-10 22:35:02 -0500

    Update ReviewList.js

--- client/src/pages/ReviewList.js
+++ client/src/pages/ReviewList.js
@@ -1,30 +1,21 @@
 import React,{useContext} from "react";
 import { MyContext } from "../components/MyContext";
-const ReviewList = () => {
-  const {reviews } = useContext(MyContext);
-  console.log(reviews)
-  
-  
-  return (
-    <div className='card'>
-    <h2 className='title'>{review.title}</h2>
-    <p>
-      <b>{review.comment}</b>
-    </p>
-    <p>
-      <b>{`Rating: ${review.rating}/5`}</b>
-    </p>
 
-    <button onClick={handleUpdateClick}>Update this review!</button>
-    <button onClick={handleDeleteClick}>Delete this review!</button>
-    {errors.map((error) => {
-      return (
-        <span key={error} className='error'>
-          {error}
-        </span>
-      );
-    })}
-  </div>
+import ReviewCard from '../components/ReviewCard.js'
+
+function ReviewList ({user}) {
+  const { reviews } = useContext(MyContext);
+
+  console.log(reviews)
+
+  return (
+    <div className='project-list'>
+      <section>
+        {reviews.map((review) => (
+          <ReviewCard key={review.id} review={review} user={user}/>
+        ))}
+      </section>
+    </div>
   );
 };
 
